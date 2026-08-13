dd72f950f6a577423b3e895536a080830fa1c55f
Revansyabian <revansyabian@gmail.com>
2026-08-13 18:34:44 +0700

    Update login.js

--- pages/login.js
+++ pages/login.js
@@ -546,7 +546,7 @@ function autoCheckSession() {
             storageRemove('sesi_pengguna');
             return;
         }
-        window.location.href = '/dashboard';
+        window.location.href = '/pages/dashboard';
     } catch (e) {
         storageRemove('sesi_pengguna');
     }
